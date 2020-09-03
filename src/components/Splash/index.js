9e66e4be1cca7726fd961d6d3ef3d17f22af0722
Mathew <mwakre29@gmail.com>
2020-09-03 16:40:02 -0700

    fetch data from numbers

--- src/components/Splash/index.js
+++ src/components/Splash/index.js
@@ -1,9 +1,31 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from "react";
 
 export default function Splash() {
-    return (
-        <div>
-            <h1>Hello</h1>
-        </div>
+  const { randomFact, setRandomFact } = useState();
+  async function fetchFacts() {
+    await fetch(
+      "https://numbersapi.p.rapidapi.com/6/21/date?fragment=true&json=true",
+      {
+        method: "GET",
+        headers: {
+          "x-rapidapi-host": "numbersapi.p.rapidapi.com",
+          "x-rapidapi-key":
+            process.env.REACT_APP_API_KEY,
+        },
+      }
     )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  return (
+    <div>
+      <h1>Hello</h1>
+      <button onClick={() => fetchFacts()}>Im a Button</button>
+    </div>
+  );
 }
