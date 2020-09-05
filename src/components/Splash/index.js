58fa5fefd750681fc31b055e45d6bd99b2bfa9e8
Mathew <mwakre29@gmail.com>
2020-09-04 17:28:42 -0700

    transfer object data into an array

--- src/components/Splash/index.js
+++ src/components/Splash/index.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
 
 export default function Splash() {
-  const { randomFact, setRandomFact } = useState({});
+  const [randomFact, setRandomFact] = useState([]);
+  // console.log('random Fact',randomFact[0].text)
+  console.log('randomFact', randomFact)
   async function fetchFacts() {
     await fetch(
       "https://numbersapi.p.rapidapi.com/6/21/date?fragment=true&json=true",
@@ -9,15 +11,19 @@ export default function Splash() {
         method: "GET",
         headers: {
           "x-rapidapi-host": "numbersapi.p.rapidapi.com",
-          "x-rapidapi-key":
-            process.env.REACT_APP_API_KEY,
+          "x-rapidapi-key": process.env.REACT_APP_API_KEY,
         },
       }
     )
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
-        setRandomFact(data)
+        for(let item in data) {
+          console.log('item', data[item])
+          setRandomFact(oldArray => [...oldArray, data[item]])
+          
+        }
+        // setRandomFact(data);
       })
       .catch((err) => {
         console.log(err);
@@ -27,6 +33,7 @@ export default function Splash() {
     <div>
       <h1>Hello</h1>
       <button onClick={() => fetchFacts()}>Im a Button</button>
+      <p>Random Fact: {randomFact[0]}</p>
     </div>
   );
 }
