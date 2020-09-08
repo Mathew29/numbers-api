27ab6eaf80bb34c81d09af529f6b84cd71abc868
Mathew <mwakre29@gmail.com>
2020-09-08 13:29:20 -0700

    and user input

--- src/components/Splash/index.js
+++ src/components/Splash/index.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
-import RandomDateFact from '../RandomDateFact/index';
+import RandomDateFact from "../RandomDateFact";
+// import RandomDateFact from '../RandomDateFact/index';
 
 export default function Splash() {
-  // const [randomFact, setRandomFact] = useState([]);
+  const [userNumber, setUserNumber] = useState([]);
   const [showFacts, setShowFacts] = useState(false);
 
   //move function to its own component
@@ -11,12 +12,13 @@ export default function Splash() {
     <div>
     <div>
       <form>
-        <input type='text'></input>
+        <input type='text' onChange={e => setUserNumber(e.target.value)}></input>
       </form>
     </div>
       <h1>Hello</h1>
-      <button onClick={() => fetchFacts()}>Im a Button</button>
-      {showFacts === true ? <p>Random Fact: {randomFact[0]}</p> : <p></p>}
+      <RandomDateFact userNumber={userNumber} setShowFacts={setShowFacts()} />
+      <button onClick={() => console.log(userNumber)}>Im a Button</button>
+      {/* {showFacts === true ? <p>Random Fact: {randomFact[0]}</p> : <p></p>} */}
       
     </div>
   );
