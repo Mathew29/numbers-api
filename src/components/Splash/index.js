4933d328bf413d0c8c939c5f3414a5a95d81c05c
Mathew <mwakre29@gmail.com>
2020-09-08 13:11:16 -0700

    transfer fetch request to new component

--- src/components/Splash/index.js
+++ src/components/Splash/index.js
@@ -1,34 +1,12 @@
 import React, { useState } from "react";
+import RandomDateFact from '../RandomDateFact/index';
 
 export default function Splash() {
-  const [randomFact, setRandomFact] = useState([]);
+  // const [randomFact, setRandomFact] = useState([]);
   const [showFacts, setShowFacts] = useState(false);
 
   //move function to its own component
-  async function fetchFacts() {
-    await fetch(
-      "https://numbersapi.p.rapidapi.com/6/21/date?fragment=true&json=true",
-      {
-        method: "GET",
-        headers: {
-          "x-rapidapi-host": "numbersapi.p.rapidapi.com",
-          "x-rapidapi-key": process.env.REACT_APP_API_KEY,
-        },
-      }
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        for (let item in data) {
-          console.log("item", data[item]);
-          setRandomFact((oldArray) => [...oldArray, data[item]]);
-        }
-        setShowFacts(true);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }
+  
   return (
     <div>
     <div>
