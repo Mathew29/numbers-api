e32e6f78843a1a9970d4cb2924ac5a6b2295e245
Mathew <mwakre29@gmail.com>
2020-09-09 11:58:08 -0700

    change form to number input type

--- src/components/Splash/index.js
+++ src/components/Splash/index.js
@@ -1,27 +1,29 @@
 import React, { useState } from "react";
 import RandomYearFact from "../RandomYearFact/index";
-import RandomMathFact from '../RandomMathFact/index';
+import RandomMathFact from "../RandomMathFact/index";
+import RandomTriviaFact from "../RandomTriviaFact/index";
 
 export default function Splash() {
   const [userNumber, setUserNumber] = useState();
-  
-
-
 
   //move function to its own component
-  
-  return (  
+
+  return (
     <div>
-    <div>
-      <form>
-        <input type='text' onChange={e => setUserNumber(e.target.value)}></input>
-      </form>
-    </div>
-      <h1>Hello</h1>
+      <h1>Welcome to the Ramdom Fact Generator</h1>
+      <div>
+        <form>
+          <input
+            type="number"
+            placeholder='Enter Number Here...'
+            onChange={(e) => setUserNumber(e.target.value)}
+          ></input>
+        </form>
+      </div>
       <RandomYearFact userNumber={userNumber} />
       <RandomMathFact userNumber={userNumber} />
+      <RandomTriviaFact userNumber={userNumber} />
       {/* <button onClick={() => console.log(userNumber)}>Im a Button</button> */}
-      
     </div>
   );
 }
